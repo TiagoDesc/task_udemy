f4dc8365be1865ba3a89cf6d90c90288688f81ca
TiagoDesc <nobretiago96@gmail.com>
2023-12-01 20:25:42 -0300

    limits2and3

--- POO/Student/Aplication.java
+++ POO/Student/Aplication.java
@@ -13,21 +13,32 @@ public class Aplication {
         grade.grade1 = sc.nextDouble();
 
         while (grade.grade1 < 0 || grade.grade1 > 30) {
-            System.out.println("Invalid Grade. Enter your grade again with a number between 0 - 30.");
+            System.out.println("Invalid Grade. Enter your grade again with a number between 0 - 30: ");
             grade.grade1 = sc.nextDouble();
         }
 
         System.out.println("Enter your grade of 2rd Trimester: ");
         grade.grade2 = sc.nextDouble();
+
+        while (grade.grade2 < 0 || grade.grade2 > 35) {
+            System.out.println("Invalid Grade. Enter your grade again with a number between 0 - 35: ");
+            grade.grade2 = sc.nextDouble();
+        }
+
         System.out.println("Enter your grade of 3rd Trimester: ");
         grade.grade3 = sc.nextDouble();
+
+        while (grade.grade3 < 0 || grade.grade3 > 35) {
+            System.out.println("Invalid Grade. Enter your grade again with a number between 0 - 35.");
+            grade.grade3 = sc.nextDouble();
+        }
 
         System.out.println("FINAL GRADE: " + grade.finalGrade());
         if (grade.finalGrade() < 60) {
             System.out.println("FAILED. ");
             System.out.println("Missing: " + grade.missingPoints() + " points.");
         } else {
-            System.out.println("PASSED. ");
+            System.out.println("PASS.");
 
         }
 
