0b74c2ffb7ca0a3a22c5a8a6e5025c425b0e660b
TiagoDesc <nobretiago96@gmail.com>
2023-12-03 20:35:53 -0300

    static circle, add class name

--- POO/Circle/Aplication.java
+++ POO/Circle/Aplication.java
@@ -6,14 +6,13 @@ public class Aplication {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Circle circle = new Circle();
 
         System.out.println("Enter radius: ");
         double radius = sc.nextDouble();
 
-        System.out.println("Circumference: " + circle.circunference(radius));
-        System.out.println("Volume: " + circle.volume(radius));
-        System.out.println("PI Value: " + circle.PI);
+        System.out.println("Circumference: " + Circle.circunference(radius));
+        System.out.println("Volume: " + Circle.volume(radius));
+        System.out.println("PI Value: " + Circle.PI);
     }
 
 }
