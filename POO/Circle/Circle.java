2b2919c23d9c322c5e7819c4779930086c92e614
TiagoDesc <nobretiago96@gmail.com>
2023-12-03 20:34:26 -0300

    stt

--- POO/Circle/Circle.java
+++ POO/Circle/Circle.java
@@ -4,11 +4,11 @@ public class Circle {
 
     public static final double PI = 3.14;
 
-    public double circunference(double radius) {
+    public static double circunference(double radius) {
         return 2 * PI * radius;
     }
 
-    public double volume(double radius) {
+    public static double volume(double radius) {
         return 4 * PI * radius * radius * radius / 3;
     }
 
