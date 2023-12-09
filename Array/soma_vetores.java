bd8582aa74b7c3bb253b533f4af4d37286b92439
TiagoDesc <nobretiago96@gmail.com>
2023-12-09 00:04:04 -0300

    program finished

--- Array/soma_vetores.java
+++ Array/soma_vetores.java
@@ -1,8 +1,35 @@
 package Array;
 
+import java.util.Scanner;
+
 public class soma_vetores {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o tamanho do vetor: ");
+        int n = sc.nextInt();
+        int vet[] = new int[n];
+        int soma = 0;
+
+        for (int i = 0; i < n; i++) {
+            System.out.printf("Vet[%d]: ", i);
+            vet[i] = sc.nextInt();
+        }
+
+        System.out.print("Valores do vetor: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(vet[i] + " ");
+        }
+
+        System.out.println();
+
+        for (int i = 0; i < n; i++) {
+            soma += vet[i];
+        }
+        System.out.println("Soma: " + soma);
+
+        double media = soma / n;
+        System.out.println("A média é: " + media);
 
     }
 
