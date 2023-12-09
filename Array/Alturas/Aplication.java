87b99c1299feced97ff3ee1e761a8339afecdb40
TiagoDesc <nobretiago96@gmail.com>
2023-12-09 00:30:20 -0300

    First changes

--- Array/Alturas/Aplication.java
+++ Array/Alturas/Aplication.java
@@ -1,5 +1,24 @@
 package Array.Alturas;
 
+import java.util.Scanner;
+
 public class Aplication {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number of people: ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Data of " + (i + 1) + " person: ");
+            System.out.print("Name: ");
+            String name = sc.nextLine();
+            sc.nextLine();
+            System.out.print("Age: ");
+            int age = sc.nextInt();
+            System.out.print("Height: ");
+            double height = sc.nextDouble();
+        }
+    }
 
 }
