44df79b890344ec30ca3b5cb60ca81af61b7fe16
TiagoDesc <nobretiago96@gmail.com>
2023-12-09 00:15:46 -0300

    Commentary

--- Array/soma_vetores.java
+++ Array/soma_vetores.java
@@ -7,29 +7,29 @@ public class soma_vetores {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Digite o tamanho do vetor: ");
-        int n = sc.nextInt();
+        int n = sc.nextInt(); // Digitando o tamanho do vetor.
         int vet[] = new int[n];
         int soma = 0;
 
         for (int i = 0; i < n; i++) {
             System.out.printf("Vet[%d]: ", i);
-            vet[i] = sc.nextInt();
+            vet[i] = sc.nextInt(); // Digitando os valores do vetor.
         }
 
         System.out.print("Valores do vetor: ");
         for (int i = 0; i < n; i++) {
-            System.out.print(vet[i] + " ");
+            System.out.print(vet[i] + " "); // Imprimindo os valores do vetor.
         }
 
         System.out.println();
 
         for (int i = 0; i < n; i++) {
-            soma += vet[i];
+            soma += vet[i]; // Realizando a soma dos valores do vetor.
         }
-        System.out.println("Soma: " + soma);
+        System.out.println("Soma: " + soma); // Imprimindo o resultado da soma.
 
         double media = soma / n;
-        System.out.println("A média é: " + media);
+        System.out.println("A média é: " + media); // Imprimindo o resultado da média.
 
     }
 
