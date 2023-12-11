2134783dcf7713b60cbd2fba5cdc72354be5e405
TiagoDesc <nobretiago96@gmail.com>
2023-12-11 00:54:39 -0300

    new question

---
+++ Array/numeros_pares.java
@@ -0,0 +1,31 @@
+package Array;
+
+import java.util.Scanner;
+
+public class numeros_pares {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the quantity numbers: ");
+        int n = sc.nextInt();
+        int vet[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter the number: ");
+            vet[i] = sc.nextInt();
+
+        }
+
+        int qttyEven = 0;
+        for (int i = 0; i < n; i++) {
+            if (vet[i] % 2 == 0) {
+                System.out.println("Even numbers: " + vet[i]);
+                qttyEven++;
+            }
+        }
+
+        System.out.println("Quantity of even numbers: " + qttyEven);
+
+    }
+
+}
