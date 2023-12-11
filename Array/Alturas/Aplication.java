7da1c7437bba4977ca5f63e48f31e140756ff48d
TiagoDesc <nobretiago96@gmail.com>
2023-12-11 00:25:06 -0300

    att commentary, the new beggin

--- Array/Alturas/Aplication.java
+++ Array/Alturas/Aplication.java
@@ -7,6 +7,7 @@ public class Aplication {
 
     public static void main(String[] args) {
 
+        // Digitar o número de pessoas.
         // Instanciar os arrays - nomes, idades, alturas.
         // Puxar os dados das pessoas - Scanner.
         // Estrutura for para verificar a quantidade de pessoas menores do que 16
@@ -23,44 +24,6 @@ public class Aplication {
 
         int n, nmenores;
         double alturatotal, alturamedia, percentualMenores;
-
-        System.out.println("Digite o número de pessoas: ");
-        n = sc.nextInt();
-
-        String nomes[] = new String[n];
-        int idades[] = new int[n];
-        double alturas[] = new double[n];
-
-        for (int i = 0; i < n; i++) {
-            System.out.printf("Dados da %da pessoa:\n", i + 1);
-            System.out.print("Nome: ");
-            nomes[i] = sc.next();
-            System.out.print("Idade: ");
-            idades[i] = sc.nextInt();
-            System.out.print("Altura: ");
-            alturas[i] = sc.nextDouble();
-        }
-
-        nmenores = 0;
-        alturatotal = 0;
-        for (int i = 0; i < n; i++) {
-            if (idades[i] < 16) {
-                nmenores++;
-            }
-            alturatotal += alturas[i];
-        }
-
-        alturamedia = (alturatotal / n);
-        percentualMenores = ((double) nmenores / n) * 100;
-
-        System.out.printf("\nAltura media = %.2f\n", alturamedia);
-        System.out.printf("Pessoas com menos de 16 anos: %.1f%%\n", percentualMenores);
-
-        for (int i = 0; i < n; i++) {
-            if (idades[i] < 16) {
-                System.out.println("Pessoas menores que 16 anos: " + nomes[i]);
-            }
-        }
 
         sc.close();
     }
