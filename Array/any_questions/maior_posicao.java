800e09243c2db8086d9d977de91dc973024b3d83
TiagoDesc <nobretiago96@gmail.com>
2023-12-13 22:42:39 -0300

    att commit

--- Array/any_questions/maior_posicao.java
+++ Array/any_questions/maior_posicao.java
@@ -1,5 +1,36 @@
 package Array.any_questions;
 
+import java.util.Scanner;
+
 public class maior_posicao {
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n, position;
+        double bigger;
+        System.out.println("How many numbers will you enter: ");
+        n = sc.nextInt();
+        double vect[] = new double[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter the number: ");
+            vect[i] = sc.nextDouble();
+        }
+
+        bigger = vect[0];
+        position = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (vect[i] > bigger) {
+                bigger = vect[i];
+                position = i;
+            }
+        }
+
+        System.out.printf("Value bigger = %.1f\n", bigger);
+        System.out.printf("Position of the highest value = %d\n", position);
+
+        sc.close();
+    }
 }
