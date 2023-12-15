51785e227117625fae7bdbcc4546ed52ff029576
TiagoDesc <nobretiago96@gmail.com>
2023-12-14 23:05:53 -0300

    executing

--- Array/any_questions/Array/any_questions/bigger_position.java
+++ Array/any_questions/Array/any_questions/bigger_position.java
@@ -2,7 +2,7 @@ package Array.any_questions;
 
 import java.util.Scanner;
 
-public class maior_posicao {
+public class bigger_position {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
