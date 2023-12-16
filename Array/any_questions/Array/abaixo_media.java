5411790eb5576dde6aa4d2ae94f1c23dcdcb816a
TiagoDesc <nobretiago96@gmail.com>
2023-12-15 23:35:30 -0300

    att

--- Array/any_questions/Array/abaixo_media.java
+++ Array/any_questions/Array/abaixo_media.java
@@ -8,15 +8,19 @@ public class abaixo_media {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Locale.setDefault(Locale.US);
-        System.out.println("Digite o valor do vetor: ");
-        int n = sc.nextInt();
         double soma = 0;
         double media = 0;
+
+        System.out.println("Digite o valor do vetor: ");
+        int n = sc.nextInt();
         double vect[] = new double[n];
 
         for (int i = 0; i < n; i++) {
-            System.out.printf("Vet[%d]: ", i);
+            System.out.println("Vect[" + (i) + "]: ");
             vect[i] = sc.nextDouble();
+        }
+
+        for (int i = 0; i < n; i++) {
             soma += vect[i];
         }
 
@@ -24,9 +28,10 @@ public class abaixo_media {
 
         System.out.println("Média do vetor: " + media);
 
+        System.out.println("Elementos abaixo da média: ");
         for (int i = 0; i < n; i++) {
             if (vect[i] < media) {
-                System.out.println("Elementos abaixo da média: " + vect[i]);
+                System.out.println(vect[i]);
             }
         }
 
