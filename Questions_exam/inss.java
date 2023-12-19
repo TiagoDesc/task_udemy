9b3b7677a61ea5e9f0d1d5700dc7518a98f51e7b
TiagoDesc <nobretiago96@gmail.com>
2023-12-18 23:46:30 -0300

    att

--- Questions_exam/inss.java
+++ Questions_exam/inss.java
@@ -1,5 +1,77 @@
 package Questions_exam;
 
+import java.util.Scanner;
+
 public class inss {
 
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        double salario;
+        double desconto;
+        double discount;
+        double result;
+        double total;
+        int user = 1;
+        while (user == 1) {
+
+            System.out.println("Digite o seu salário: ");
+            salario = entrada.nextDouble();
+
+            if (salario <= 1903.98) {
+                System.out.println("Seu salário é de: R$" + salario);
+                System.out.println("Você está isento dos impostos!");
+                System.out.println("Deseja fazer uma nova consulta? 1 - Sim, 2 - Não.");
+                user = entrada.nextInt();
+
+            } else if (salario >= 1903.99 && salario <= 2826.65) {
+                desconto = 0.075;
+                discount = salario * desconto;
+                total = salario - discount;
+                System.out.println("Seu salário é de: R$" + salario);
+                System.out.printf("Você teve um desconto de R$ %.2f", discount);
+                System.out.println(" ");
+                System.out.printf("Seu salário líquido será de: R$ %.2f", total);
+                System.out.println("");
+                System.out.println("Deseja fazer uma nova consulta? 1 - Sim, 2 - Não.");
+                user = entrada.nextInt();
+
+            } else if (salario >= 2826.66 && salario <= 3751.05) {
+                desconto = 0.15;
+                discount = salario * desconto;
+                total = salario - discount;
+                System.out.println("Seu salário é de: R$" + salario);
+                System.out.printf("Você teve um desconto de R$ %.2f", discount);
+                System.out.println(" ");
+                System.out.printf("Seu salário líquido será de: R$ %.2f", total);
+                System.out.println();
+                System.out.println("Deseja fazer uma nova consulta? 1 - Sim, 2 - Não.");
+                user = entrada.nextInt();
+
+            } else if (salario >= 3751.06 && salario <= 4664.68) {
+                desconto = 0.225;
+                discount = salario * desconto;
+                total = salario - discount;
+                System.out.println("Seu salário é de: R$" + salario);
+                System.out.printf("Você teve um desconto de R$ %.2f", discount);
+                System.out.println(" ");
+                System.out.printf("Seu salário líquido será de: R$ %.2f ", total);
+                System.out.println();
+                System.out.println("Deseja fazer uma nova consulta? 1 - Sim, 2 - Não.");
+                user = entrada.nextInt();
+
+            } else if (salario >= 4644.69) {
+                desconto = 0.275;
+                discount = salario * desconto;
+                total = salario - discount;
+                System.out.println("Seu salário é de: R$" + salario);
+                System.out.printf("Você teve um desconto de R$ %.2f", discount);
+                System.out.println(" ");
+                System.out.printf("Seu salário líquido será de: R$ %.2f", total);
+                System.out.println();
+                System.out.println("Deseja fazer uma nova consulta? 1 - Sim, 2 - Não.");
+                user = entrada.nextInt();
+
+            }
+        }
+    }
 }
