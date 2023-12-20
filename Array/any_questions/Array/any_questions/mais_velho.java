b93e152f63f16a8ad416bbc2cc3772fdf8d7ee52
TiagoDesc <nobretiago96@gmail.com>
2023-12-19 22:16:53 -0300

    new commit

---
+++ Array/any_questions/Array/any_questions/mais_velho.java
@@ -0,0 +1,14 @@
+package Array.any_questions;
+
+import java.util.Scanner;
+
+public class mais_velho {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        sc.close();
+        ;
+    }
+
+}
