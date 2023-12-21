61ab285d0acea07d1b4fd2efb3b9cdd3ae7d228f
TiagoDesc <nobretiago96@gmail.com>
2023-12-20 22:12:04 -0300

    inicializing for

--- Array/any_questions/Array/any_questions/people_data.java
+++ Array/any_questions/Array/any_questions/people_data.java
@@ -1,5 +1,17 @@
 package Array.any_questions;
 
+import java.util.Scanner;
+
 public class people_data {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of people: ");
+        int n = sc.nextInt();
+        double height[] = new double[n];
+        char gender[] = new char[n];
+
+        sc.close();
+    }
 
 }
