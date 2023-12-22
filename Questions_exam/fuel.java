02f401018f24f68b04e3c82803764276d7c3fdd4
TiagoDesc <nobretiago96@gmail.com>
2023-12-21 23:49:03 -0300

    import locale

--- Questions_exam/fuel.java
+++ Questions_exam/fuel.java
@@ -1,10 +1,12 @@
 package Questions_exam;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class fuel {
 
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
         double valueTotal, discount = 0;
         double liters_gasoline;
