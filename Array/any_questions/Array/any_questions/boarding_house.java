8b9041e0cece0fb3690f1d862ecacfeaf7a4e5a4
TiagoDesc <nobretiago96@gmail.com>
2023-12-26 23:12:44 -0300

    new att

--- Array/any_questions/Array/any_questions/boarding_house.java
+++ Array/any_questions/Array/any_questions/boarding_house.java
@@ -1,5 +1,34 @@
 package Array.any_questions;
 
+import java.util.Scanner;
+
 public class boarding_house {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("How many rooms will be ranted? ");
+        int n = sc.nextInt();
+        String name[] = new String[n];
+        String email[] = new String[n];
+        int room[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Rent " + (i + 1) + "#:");
+            System.out.print("Name: ");
+            sc.nextLine();
+            name[i] = sc.nextLine();
+            System.out.print("Email: ");
+            email[i] = sc.nextLine();
+            System.out.print("Room: ");
+            room[i] = sc.nextInt();
+
+        }
+
+        System.out.println("Busy Room: ");
+        for (int i = 0; i < n; i++) {
+            System.out.println(room[i] + name[i] + email[i]);
+        }
+
+    }
 
 }
