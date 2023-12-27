c5c0a343d88d77448eaabb625e0fcc5ba5155d71
TiagoDesc <nobretiago96@gmail.com>
2023-12-26 23:20:30 -0300

    add constructor

--- Array/ArrayPOO/Boarding/Rent.java
+++ Array/ArrayPOO/Boarding/Rent.java
@@ -5,6 +5,10 @@ public class Rent {
     private String name;
     private String email;
 
+    public Rent() {
+
+    }
+
     public Rent(String name, String email) {
         this.name = name;
         this.email = email;
