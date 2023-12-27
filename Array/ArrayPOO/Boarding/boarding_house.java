2631568c7506828453396982ad8012679a8b425a
TiagoDesc <nobretiago96@gmail.com>
2023-12-26 23:52:17 -0300

    finish

--- Array/ArrayPOO/Boarding/boarding_house.java
+++ Array/ArrayPOO/Boarding/boarding_house.java
@@ -6,27 +6,33 @@ public class boarding_house {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        Rent[] vect = new Rent[10];
+
         System.out.print("How many rooms will be ranted? ");
         int n = sc.nextInt();
-        String name[] = new String[n];
-        String email[] = new String[n];
-        int room[] = new int[n];
 
-        for (int i = 0; i < n; i++) {
-            System.out.println("Rent " + (i + 1) + "#:");
+        for (int i = 1; i <= n; i++) {
+            System.out.println();
+            System.out.println("Rent #" + i + ":");
             System.out.print("Name: ");
             sc.nextLine();
-            name[i] = sc.nextLine();
-            System.out.print("Email: ");
-            email[i] = sc.nextLine();
-            System.out.print("Room: ");
-            room[i] = sc.nextInt();
+            String name = sc.nextLine();
+            System.out.println("Email: ");
+            String email = sc.nextLine();
+            System.out.println("Room: ");
+            int roomNumber = sc.nextInt();
 
+            // Rent rent = new Rent(name, email); "Draft".
+            vect[roomNumber] = new Rent(name, email);
         }
 
-        System.out.println("Busy Room: ");
-        for (int i = 0; i < n; i++) {
-            System.out.println(room[i] + name[i] + email[i]);
+        System.out.println();
+        System.out.println("Busy rooms: ");
+        for (int i = 0; i < 10; i++) {
+            if (vect[i] != null) {
+                System.out.println(i + ": " + vect[i]);
+            }
         }
 
     }
