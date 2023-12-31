26172b0bf4ef1d1d12906f18880a3ed3945aa322
TiagoDesc <nobretiago96@gmail.com>
2023-12-30 23:55:05 -0300

    New Array

--- Listas/Employee/Aplication.java
+++ Listas/Employee/Aplication.java
@@ -1,5 +1,7 @@
 package Listas.Employee;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -8,6 +10,9 @@ public class Aplication {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+
+        List<Employee> list = new ArrayList<>();
+        System.out.print("How many employees will be registered?");
 
     }
 
