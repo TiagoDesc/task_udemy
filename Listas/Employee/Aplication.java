9dea4c6901da9ff3ae2cc187e4837634696d6c36
TiagoDesc <nobretiago96@gmail.com>
2023-12-30 23:53:13 -0300

    import

--- Listas/Employee/Aplication.java
+++ Listas/Employee/Aplication.java
@@ -1,8 +1,13 @@
 package Listas.Employee;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Aplication {
 
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
 
     }
 
