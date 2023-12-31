bbc434a823b4575d60a64af51d71ef93bb4fd764
TiagoDesc <nobretiago96@gmail.com>
2023-12-30 23:43:18 -0300

    getters and setters

--- Listas/Employee/Employee.java
+++ Listas/Employee/Employee.java
@@ -16,4 +16,28 @@ public class Employee {
         this.salary = salary;
     }
 
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
 }
