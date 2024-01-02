fdc2a6dfc572ab70732d114ef6a5332ff7d7455a
TiagoDesc <nobretiago96@gmail.com>
2024-01-01 22:43:36 -0300

    matriz

--- Matriz/ordem.java
+++ Matriz/ordem.java
@@ -1,5 +1,15 @@
 package Matriz;
 
+import java.util.Scanner;
+
 public class ordem {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter with number: ");
+        int n = sc.nextInt();
+        int[][] matriz = new int[n][n];
+
+    }
 
 }
