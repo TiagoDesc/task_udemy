6e5678e6a74564c815e6ee45a9a0ab3a6118b538
TiagoDesc <nobretiago96@gmail.com>
2024-01-01 23:06:41 -0300

    final commit

--- Matriz/ordem.java
+++ Matriz/ordem.java
@@ -5,35 +5,34 @@ import java.util.Scanner;
 public class ordem {
 
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter with number: ");
         int n = sc.nextInt();
-        int[][] matriz = new int[n][n];
+        int[][] mat = new int[n][n];
 
-        for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[i].length; j++) {
-                matriz[i][j] = sc.nextInt();
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[i].length; j++) {
+                mat[i][j] = sc.nextInt();
             }
-
         }
 
-        System.out.println("Main diagonal: ");
-        for (int i = 0; i < matriz.length; i++) {
-            System.out.print(matriz[i][i] + " ");
+        System.out.println("Main diagonal:");
+        for (int i = 0; i < mat.length; i++) {
+            System.out.print(mat[i][i] + " ");
         }
+        System.out.println();
 
         int count = 0;
-        for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[i].length; i++) {
-                if (matriz[i][j] < 0) {
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[i].length; j++) {
+                if (mat[i][j] < 0) {
                     count++;
                 }
             }
         }
+        System.out.println("Negative numbers = " + count);
 
-        System.out.println("Quantity of negative numbers is: " + count);
-
+        sc.close();
     }
-
 }
