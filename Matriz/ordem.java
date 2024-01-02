35f3cff7fda828169ede8685fd31794906fb5151
TiagoDesc <nobretiago96@gmail.com>
2024-01-01 23:01:48 -0300

    finish

--- Matriz/ordem.java
+++ Matriz/ordem.java
@@ -6,23 +6,33 @@ public class ordem {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter with number: ");
+
+        System.out.print("Enter with number: ");
         int n = sc.nextInt();
         int[][] matriz = new int[n][n];
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                System.out.println("Enter: ");
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
                 matriz[i][j] = sc.nextInt();
             }
 
         }
 
-        for (int i = 0; i < n; i++) {
+        System.out.println("Main diagonal: ");
+        for (int i = 0; i < matriz.length; i++) {
             System.out.print(matriz[i][i] + " ");
         }
 
         int count = 0;
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; i++) {
+                if (matriz[i][j] < 0) {
+                    count++;
+                }
+            }
+        }
+
+        System.out.println("Quantity of negative numbers is: " + count);
 
     }
 
