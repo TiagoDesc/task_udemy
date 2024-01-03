021fbfa9659e3c6fec88f7ccb370ef80c937890a
TiagoDesc <nobretiago96@gmail.com>
2024-01-02 23:35:04 -0300

    att

--- Matriz/line_column.java
+++ Matriz/line_column.java
@@ -1,5 +1,36 @@
 package Matriz;
 
+import java.util.Scanner;
+
 public class line_column {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number: ");
+        int m = sc.nextInt();
+
+        System.out.print("Enter the number: ");
+        int n = sc.nextInt();
+
+        int[][] matriz = new int[m][n];
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                matriz[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Insert a number that belongs to the matrix: ");
+        int x = sc.nextInt();
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                if (matriz[i][j] == x) {
+                    System.out.println("P");
+                }
+            }
+        }
+    }
 
 }
