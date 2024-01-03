547aa0a69617454342fb834da3bd5b9ccefd4e42
TiagoDesc <nobretiago96@gmail.com>
2024-01-03 01:03:16 -0300

    att

--- Matriz/line_column.java
+++ Matriz/line_column.java
@@ -19,6 +19,7 @@ public class line_column {
         for (int i = 0; i < matriz.length; i++) {
             for (int j = 0; j < matriz[i].length; j++) {
                 matriz[i][j] = sc.nextInt();
+                System.out.println();
             }
         }
 
