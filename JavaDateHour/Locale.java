dfeb1a3d22961a6b72a8f79c29d10621f5cd8180
TiagoDesc <nobretiago96@gmail.com>
2024-01-04 13:58:47 -0300

    d08

--- JavaDateHour/Locale.java
+++ JavaDateHour/Locale.java
@@ -3,10 +3,15 @@ package JavaDateHour;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import javax.swing.text.DateFormatter;
 
 public class Locale {
 
     public static void main(String[] args) {
+
+        DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
         LocalDate d01 = LocalDate.now();
         LocalDateTime d02 = LocalDateTime.now();
         Instant d03 = Instant.now();
@@ -16,6 +21,8 @@ public class Locale {
         Instant d06 = Instant.parse("2022-07-20T01:30:26Z");
         Instant d07 = Instant.parse("2022-07-20T01:30:26-03:00");
 
+        LocalDate d08 = LocalDate.parse("20-07-2022", fmt1);
+
         System.out.println("d01 = " + d01.toString());
         System.out.println("d02 = " + d02.toString());
         System.out.println("d03 = " + d03.toString());
@@ -23,6 +30,7 @@ public class Locale {
         System.out.println("d05 = " + d05.toString());
         System.out.println("d06 = " + d06.toString());
         System.out.println("d07 = " + d07.toString());
+        System.out.println("d08 = " + d08.toString());
 
     }
 
