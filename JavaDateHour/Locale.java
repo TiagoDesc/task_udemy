9160da795a00930c9d4c57977ea7ab5f80c78d06
TiagoDesc <nobretiago96@gmail.com>
2024-01-03 23:06:09 -0300

    Instant

--- JavaDateHour/Locale.java
+++ JavaDateHour/Locale.java
@@ -1,5 +1,6 @@
 package JavaDateHour;
 
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
@@ -8,6 +9,7 @@ public class Locale {
     public static void main(String[] args) {
         LocalDate d01 = LocalDate.now();
         LocalDateTime d02 = LocalDateTime.now();
+        Instant d03 = Instant.now();
 
     }
 
