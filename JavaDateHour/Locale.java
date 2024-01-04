cd9904f97e2e0e8bda8df4dd39c94ca0b9e848c0
TiagoDesc <nobretiago96@gmail.com>
2024-01-03 23:05:37 -0300

    d02

--- JavaDateHour/Locale.java
+++ JavaDateHour/Locale.java
@@ -1,11 +1,14 @@
 package JavaDateHour;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class Locale {
 
     public static void main(String[] args) {
         LocalDate d01 = LocalDate.now();
+        LocalDateTime d02 = LocalDateTime.now();
+
     }
 
 }
