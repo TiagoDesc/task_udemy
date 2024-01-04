0d0858473c8a818cbb34d9a99178d54290db0cea
TiagoDesc <nobretiago96@gmail.com>
2024-01-04 13:45:50 -0300

    d06

--- JavaDateHour/Locale.java
+++ JavaDateHour/Locale.java
@@ -13,12 +13,14 @@ public class Locale {
 
         LocalDate d04 = LocalDate.parse("2022-07-20");
         LocalDateTime d05 = LocalDateTime.parse("2022-07-20T01:30:26");
+        Instant d06 = Instant.parse("2022-07-20T01:30:26Z");
 
         System.out.println("d01 = " + d01.toString());
         System.out.println("d02 = " + d02.toString());
         System.out.println("d03 = " + d03.toString());
         System.out.println("d04 = " + d04.toString());
         System.out.println("d05 = " + d05.toString());
+        System.out.println("d06 = " + d06.toString());
 
     }
 
