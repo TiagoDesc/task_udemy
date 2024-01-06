543a5fee2eb7980405fe4122c100c69ba78df1f1
TiagoDesc <nobretiago96@gmail.com>
2024-01-05 22:08:53 -0300

    d04

--- JavaDateHour/ConverterDate.java
+++ JavaDateHour/ConverterDate.java
@@ -1,5 +1,12 @@
 package JavaDateHour;
 
+import java.time.LocalDate;
+
 public class ConverterDate {
+
+    public static void main(String[] args) {
+
+        LocalDate d04 = LocalDate.parse("2022-07-20");
+    }
 
 }
