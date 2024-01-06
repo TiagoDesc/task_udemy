779ae5f4da7360eca71a56b5a04d31ade80025c3
TiagoDesc <nobretiago96@gmail.com>
2024-01-05 22:14:33 -0300

    ZoneID+

--- JavaDateHour/ConverterDate.java
+++ JavaDateHour/ConverterDate.java
@@ -3,6 +3,7 @@ package JavaDateHour;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
 public class ConverterDate {
@@ -14,9 +15,11 @@ public class ConverterDate {
         Instant d06 = Instant.parse("2022-07-20T01:30:26Z");
 
         DateTimeFormatter fmt1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter fmt2 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        DateTimeFormatter fmt3 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm").withZone(ZoneId.systemDefault());
 
-        System.out.println("d04 = " + d04);
-        System.out.println("d05 = " + d05);
+        System.out.println("d04 = " + d04.format(fmt1));
+        System.out.println("d05 = " + fmt1.format(d04));
         System.out.println("d06 = " + d06);
     }
 
