a84092f0231427130e316aec6002f2b7db721934
TiagoDesc <nobretiago96@gmail.com>
2024-01-08 21:58:46 -0300

    d05

--- JavaDateHour/Week.java
+++ JavaDateHour/Week.java
@@ -1,5 +1,18 @@
 package JavaDateHour;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Week {
 
+    public static void main(String[] args) {
+
+        LocalDate d04 = LocalDate.parse("2022-07-20");
+        LocalDateTime d05 = LocalDateTime.parse("2022-07-20T01:30:26");
+        Instant d06 = Instant.parse("2022-07-20T01:30:26Z");
+
+        LocalDateTime pastWeekLocalDate = d05.minusDays(7);
+
+    }
 }
