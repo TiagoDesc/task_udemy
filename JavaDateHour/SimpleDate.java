8a077580bd4db28fb3fce3eab3b685363da5c5e6
TiagoDesc <nobretiago96@gmail.com>
2024-01-12 00:23:06 -0300

    att

--- JavaDateHour/SimpleDate.java
+++ JavaDateHour/SimpleDate.java
@@ -2,6 +2,7 @@ package JavaDateHour;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.Instant;
 import java.util.Date;
 
 public class SimpleDate {
@@ -11,11 +12,22 @@ public class SimpleDate {
         SimpleDateFormat sdf1 = new SimpleDateFormat("dd/MM/yyyy");
         SimpleDateFormat sdf2 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 
+        Date x1 = new Date();
+        Date x2 = new Date(System.currentTimeMillis());
+        Date x3 = new Date(0L);
+        Date x4 = new Date(1000L * 60 * 60 * 5);
+
         Date y1 = sdf1.parse("12/01/2024");
         Date y2 = sdf2.parse("12/01/2024 00:09:20");
+        Date y3 = Date.from(Instant.parse("2018-06-25T15:42:07Z"));
 
+        System.out.println("x1: " + sdf2.format(x1));
+        System.out.println("x2: " + sdf2.format(x2));
+        System.out.println("x3: " + sdf2.format(x3));
+        System.out.println("x4: " + sdf2.format(x4));
         System.out.println("Y1 = " + sdf2.format(y1));
         System.out.println("Y2 = " + sdf2.format(y2));
+        System.out.println("Y3 = " + sdf2.format(y3));
     }
 
 }
