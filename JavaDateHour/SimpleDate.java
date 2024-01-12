8f310cc7410f04aba9a56bd1937449e9f6ad7f75
TiagoDesc <nobretiago96@gmail.com>
2024-01-12 00:07:01 -0300

    sdf1

--- JavaDateHour/SimpleDate.java
+++ JavaDateHour/SimpleDate.java
@@ -1,5 +1,13 @@
 package JavaDateHour;
 
+import java.text.SimpleDateFormat;
+
 public class SimpleDate {
+
+    public static void main(String[] args) {
+
+        SimpleDateFormat sdf1 = new SimpleDateFormat("dd/MM/yyyy");
+
+    }
 
 }
