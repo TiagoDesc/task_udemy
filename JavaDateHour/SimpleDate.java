97dc3de6834b91b9cb89e7da0cdad04535f1a253
TiagoDesc <nobretiago96@gmail.com>
2024-01-12 00:09:25 -0300

    sdf2

--- JavaDateHour/SimpleDate.java
+++ JavaDateHour/SimpleDate.java
@@ -1,13 +1,18 @@
 package JavaDateHour;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class SimpleDate {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
 
         SimpleDateFormat sdf1 = new SimpleDateFormat("dd/MM/yyyy");
+        SimpleDateFormat sdf2 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 
+        Date y1 = sdf1.parse("12/01/2024");
+        Date y2 = sdf1.parse("12/01/2024 00:09:20");
     }
 
 }
