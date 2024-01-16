a32b743879811886e95a1b4faba9972d5e14c975
TiagoDesc <nobretiago96@gmail.com>
2024-01-15 23:35:55 -0300

    closed

--- POO/Rectangle/aplication.java
+++ POO/Rectangle/aplication.java
@@ -1,9 +1,11 @@
 package POO.Rectangle;
+
 import java.util.Scanner;
+
 public class aplication {
-    
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
+        Scanner sc = new Scanner(System.in);
         rectangle rectangle = new rectangle();
         System.out.println("Enter rectangle width and height: ");
 
@@ -13,11 +15,13 @@ public class aplication {
         System.out.println("Enter the height: ");
         rectangle.height = sc.nextDouble();
 
-        System.out.println("Area: " +  rectangle.Area());
+        System.out.println("Area: " + rectangle.Area());
 
         System.out.println("Perimeter: " + rectangle.Perimeter());
 
         System.out.println("Diagonal: " + rectangle.Diagonal());
-        
+
+        sc.close();
+
     }
 }
