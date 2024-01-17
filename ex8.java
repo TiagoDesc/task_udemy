5334e8d0247ae2b758e741926075b8a72574dfd4
TiagoDesc <nobretiago96@gmail.com>
2024-01-16 23:53:57 -0300

    att

--- ex8.java
+++ ex8.java
@@ -1,22 +1,22 @@
 import java.util.Scanner;
+
 public class ex8 {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
-        
+        Scanner sc = new Scanner(System.in);
+
         int senha = 2002;
         int user;
         System.out.println("Enter the password: ");
         user = sc.nextInt();
 
-        while(user != senha){                
-            System.out.println("Senha incorreta. Digite novamente: ");
+        while (user != senha) {
+            System.out.println("Incorrect password. Enter again: ");
             user = sc.nextInt();
         }
 
-        System.out.println("Acesso permitido.");
+        System.out.println("Access allowed.");
         sc.close();
     }
-    
-     
+
 }
