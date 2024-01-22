abdc3c304103b31878db07c4adadda96ee612462
TiagoDesc <nobretiago96@gmail.com>
2024-01-21 23:29:03 -0300

    att

--- ex3.java
+++ ex3.java
@@ -3,14 +3,15 @@ import java.util.Scanner;
 public class ex3 {
 
     public static void main(String[] args) {
-        Scanner entrada = new Scanner (System.in);
+        Scanner entrada = new Scanner(System.in);
         System.out.println("Digite um número e descubra se ele é par ou impar: ");
         int num = entrada.nextInt();
 
-        if(num % 2 == 0){
+        if (num % 2 == 0) {
             System.out.println(num + " é par.");
-        }else if(num % 2 != 0){
+        } else if (num % 2 != 0) {
             System.out.println(num + " é impar.");
+        }
+
     }
-    
 }
