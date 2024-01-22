78dbb68aed65d15717023c83197ac58bd3eda9c6
TiagoDesc <nobretiago96@gmail.com>
2024-01-21 23:35:27 -0300

    new commit

--- ex10_List1.java
+++ ex10_List1.java
@@ -1,3 +1,23 @@
+import java.util.Scanner;
+
 public class ex10_List1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numberEmployee;
+        int workedHours;
+        double valueHours;
+        System.out.println("Enter the number of employee: ");
+        numberEmployee = sc.nextInt();
+        System.out.println("Enter the worked hours: ");
+        workedHours = sc.nextInt();
+        System.out.println("Enter the value hours: ");
+        valueHours = sc.nextDouble();
+
+        double result = (double) workedHours * valueHours;
+
+        System.out.println("The employee number " + numberEmployee + " have a salary of: $" + result);
+
+    }
 
 }
