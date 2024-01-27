942b738d0a68cc2f6fbc9b8782f9f189ce8011f6
Tiago Nobre <132406448+TiagoDesc@users.noreply.github.com>
2024-01-26 23:52:48 -0300

    Update ex2.java

--- ex2.java
+++ ex2.java
@@ -5,7 +5,7 @@ public class ex2 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
 
-        System.out.println("Digite um número: ");
+        System.out.println("Enter the number: ");
         int num = entrada.nextInt();
         if (num < 0) {
             System.out.println(num + " é negativo.");
