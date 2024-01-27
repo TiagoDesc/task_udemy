4336da7ba02761e0a25194ac9b4da4a73a636a6a
Tiago Nobre <132406448+TiagoDesc@users.noreply.github.com>
2024-01-26 23:53:32 -0300

    Update ex2.java

--- ex2.java
+++ ex2.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 public class ex2 {
 
     public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter the number: ");
-        int num = entrada.nextInt();
+        int num = sc.nextInt();
         if (num < 0) {
-            System.out.println(num + " é negativo.");
+            System.out.println(num + " is negative.");
         } else if (num >= 0) {
-            System.out.println(num + " não é negativo.");
+            System.out.println(num + " is not negative.");
         }
     }
 
