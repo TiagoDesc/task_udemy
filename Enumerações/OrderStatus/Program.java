36a52fe562e8bbe83c3a38db3112bd02fbcbfe52
TiagoDesc <nobretiago96@gmail.com>
2024-02-14 16:05:11 -0300

    new

--- Enumerações/OrderStatus/Program.java
+++ Enumerações/OrderStatus/Program.java
@@ -10,7 +10,7 @@ public class Program {
         System.out.println(order);
 
         OrderStatus os1 = OrderStatus.DELIVERED;
-        OrderStatus os2 = OrderStatus.valueOf("Delivered");
+        OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
 
         System.out.println(os1);
         System.out.println(os2);
