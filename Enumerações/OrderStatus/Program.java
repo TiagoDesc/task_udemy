cd68289b2ca99802b36d99de3620cd2fbba5e116
Tiago Nobre <132406448+TiagoDesc@users.noreply.github.com>
2024-02-16 22:20:41 -0300

    att

--- Enumerações/OrderStatus/Program.java
+++ Enumerações/OrderStatus/Program.java
@@ -11,6 +11,8 @@ public class Program {
 
         OrderStatus os1 = OrderStatus.DELIVERED;
         OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
+        OrderStatus os3 = OrderStatus.PROCESSING;
+        OrderStatus os4 = OrderStatus.ValueOf("PROCESSING");
 
         System.out.println(os1);
         System.out.println(os2);
