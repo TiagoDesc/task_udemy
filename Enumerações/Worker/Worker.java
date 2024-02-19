625248fd2e2726a3c2bd386ff0e8de6d10d7a26b
TiagoDesc <nobretiago96@gmail.com>
2024-02-18 22:57:46 -0300

    new

--- Enumerações/Worker/Worker.java
+++ Enumerações/Worker/Worker.java
@@ -1,5 +1,26 @@
 package Enumerações.Worker;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Worker {
+
+    private String name;
+    private WorkerLevel level;
+    private Double baseSalary;
+
+    private Department department;
+    private List<HourContract> contracts = new ArrayList<>();
+
+    public Worker() {
+
+    }
+
+    public Worker(String name, WorkerLevel level, Double baseSalary, Department department) {
+        this.name = name;
+        this.level = level;
+        this.baseSalary = baseSalary;
+        this.department = department;
+    }
 
 }
