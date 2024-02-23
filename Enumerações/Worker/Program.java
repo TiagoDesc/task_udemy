382f502494f75657e7d73b0751f1fb03fce20fb4
TiagoDesc <nobretiago96@gmail.com>
2024-02-22 22:50:27 -0300

    closed

--- Enumerações/Worker/Program.java
+++ Enumerações/Worker/Program.java
@@ -48,6 +48,8 @@ public class Program {
         System.out.println("Name: " + worker.getName());
         System.out.println("Department " + worker.getDepartment().getName());
         System.out.println("Income for " + monthAndYear + ": " + String.format("%.2f", worker.income(year, month)));
+
+        sc.close();
     }
 
 }
