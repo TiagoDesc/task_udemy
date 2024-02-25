9ab4806259199bc449fc13fb196bea8ec7bee516
TiagoDesc <nobretiago96@gmail.com>
2024-02-24 22:18:16 -0300

    new

--- POO/Biblioteca/Autor.java
+++ POO/Biblioteca/Autor.java
@@ -1,5 +1,50 @@
 package POO.Biblioteca;
 
 public class Autor {
+    private String nome;
+    private int idade;
+    private String nacionalidade;
 
+    public Autor() {
+
+    }
+
+    public Autor(String nome, int idade, String nacionalidade) {
+        this.nome = nome;
+        this.idade = idade;
+        this.nacionalidade = nacionalidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+    @Override
+    public String toString() {
+        return "Autor{" +
+                "nome='" + nome + '\'' +
+                ", idade=" + idade +
+                ", nacionalidade='" + nacionalidade + '\'' +
+                '}';
+    }
 }
