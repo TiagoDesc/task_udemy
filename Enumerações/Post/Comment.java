4d4baa5499426ebd4d2e1c73335408ecfac73668
TiagoDesc <nobretiago96@gmail.com>
2024-02-26 15:57:24 -0300

    new commit

---
+++ Enumerações/Post/Comment.java
@@ -0,0 +1,23 @@
+package Enumerações.Post;
+
+public class Comment {
+
+    public String text;
+
+    public Comment() {
+
+    }
+
+    public Comment(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+}
