55e31453d312429a380448f0eda9311eef042f7c
Tiago Nobre <132406448+TiagoDesc@users.noreply.github.com>
2024-02-27 20:10:32 -0300

    sdf

--- Enumerações/Post/Program.java
+++ Enumerações/Post/Program.java
@@ -3,6 +3,9 @@ package Enumerações.Post;
 public class Program {
 
     public static void main(String[] args) {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        
         Comment c1 = new Comment("Have a nice trip!");
 
     }
