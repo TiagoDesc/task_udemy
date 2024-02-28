258a2e47f2272761a75c1730622cd8b6ed11416b
TiagoDesc <nobretiago96@gmail.com>
2024-02-28 19:01:55 -0300

    new commit

--- Enumerações/Post/Program.java
+++ Enumerações/Post/Program.java
@@ -1,11 +1,13 @@
 package Enumerações.Post;
 
+import java.text.SimpleDateFormat;
+
 public class Program {
 
     public static void main(String[] args) {
 
         SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-        
+
         Comment c1 = new Comment("Have a nice trip!");
 
     }
