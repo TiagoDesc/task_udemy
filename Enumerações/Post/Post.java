4d89c4aab407534f41b231fa1ec39231c30c8d56
TiagoDesc <nobretiago96@gmail.com>
2024-02-28 19:17:46 -0300

    May the force be with you

--- Enumerações/Post/Post.java
+++ Enumerações/Post/Post.java
@@ -1,10 +1,13 @@
 package Enumerações.Post;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 public class Post {
+
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 
     private Date moment;
     private String title;
@@ -69,17 +72,17 @@ public class Post {
     }
 
     public String toString() {
-		StringBuilder sb = new StringBuilder();
-		sb.append(title + "\n");
-		sb.append(likes);
-		sb.append(" Likes - ");
-		sb.append(sdf.format(moment) + "\n");
-		sb.append(content + "\n");
-		sb.append("Comments:\n");
-		for (Comment c : comments) {
-			sb.append(c.getText() + "\n");
-		}
-		return sb.toString();
-	}
+        StringBuilder sb = new StringBuilder();
+        sb.append(title + "\n");
+        sb.append(likes);
+        sb.append(" Likes - ");
+        sb.append(sdf.format(moment) + "\n");
+        sb.append(content + "\n");
+        sb.append("Comments:\n");
+        for (Comment c : comments) {
+            sb.append(c.getText() + "\n");
+        }
+        return sb.toString();
+    }
 
 }
