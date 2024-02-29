9b0179b9296846b744881918b483f7907d6b2b1e
TiagoDesc <nobretiago96@gmail.com>
2024-02-29 16:00:27 -0300

    later I'll finish :)

--- Enumerações/OrderTask/Program.java
+++ Enumerações/OrderTask/Program.java
@@ -1,11 +1,20 @@
 package Enumerações.OrderTask;
 
 import java.text.SimpleDateFormat;
+import java.util.Scanner;
 
 public class Program {
 
     public static void main(String[] args) {
-        SimpleDateFormat sdf = new SimpleDateFormat();
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
+        System.out.println("Enter client data: ");
+        System.out.print("Name: ");
+        String name = sc.nextLine();
+        System.out.print("Email: ");
+        String email = sc.nextLine();
+        // Date date = sc.nextByte(sdf.parse(email));
 
     }
 
