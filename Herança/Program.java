58ee88881cdd006a724944e3ffb8f70d7cc3b704
Tiago Nobre <132406448+TiagoDesc@users.noreply.github.com>
2024-03-08 21:56:26 -0300

    Update Program.java

--- Herança/Program.java
+++ Herança/Program.java
@@ -4,7 +4,7 @@ public class Program {
 
     public static void main(String[] args) {
 
-        BusinessAccount account = new BusinessAccount(1001, "Tiago", 1000.0, 500.0);
+        BusinessAccount account = new BusinessAccount(1001, "Tiago", 1000.0, 800.0);
 
         System.out.println(account.getBalance());
 
