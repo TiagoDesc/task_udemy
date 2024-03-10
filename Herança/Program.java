acc2cabd2c55f0d28e5e8bad54144a1ecf8d13d1
TiagoDesc <nobretiago96@gmail.com>
2024-03-10 00:05:43 -0300

    att

--- Herança/Program.java
+++ Herança/Program.java
@@ -1,17 +1,30 @@
 package Herança;
 
+import java.util.Scanner;
+
 public class Program {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        BusinessAccount account = new BusinessAccount(1001, "Tiago", 1000.0, 800.0);
+        System.out.println("Number: ");
+        Integer number = sc.nextInt();
+        System.out.println("Holder: ");
+        sc.next();
+        String holder = sc.nextLine();
+        System.out.println("Balance:");
+        Double balance = sc.nextDouble();
+        System.out.println("Loan limit: ");
+        Double loanLimit = sc.nextDouble();
+
+        BusinessAccount account = new BusinessAccount(number, holder, balance, loanLimit);
 
         System.out.println("The order: ");
         System.out.println(account.getNumber());
-        System.out.println(account.getHolder());
+
         System.out.println(account.getBalance());
         System.out.println(account.getLoanLimit());
-
+        System.out.println(account.getHolder());
     }
 
 }
