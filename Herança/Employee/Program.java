f5916a464dd39d336e07650337fdebff58c17591
TiagoDesc <nobretiago96@gmail.com>
2024-03-11 16:41:50 -0300

    new

--- Herança/Employee/Program.java
+++ Herança/Employee/Program.java
@@ -1,13 +1,52 @@
 package Herança.Employee;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
+import POO.Employee.employee;
 
 public class Program {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the number of employees: ");
 
+        List<Employee> list = new ArrayList<Employee>();
+
+        System.out.print("Enter the number of employees: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print("Employee #" + i + " data:");
+            System.out.print("Outsourced (y/n)?");
+            char answer = sc.next().charAt(0);
+            System.out.print("Name: ");
+            sc.nextLine();
+            String name = sc.nextLine();
+            System.out.print("Hours: ");
+            int hours = sc.nextInt();
+            System.out.print("Value per hour: ");
+            double valuePerHour = sc.nextDouble();
+            if (answer == 'y' || answer == 'Y') {
+                System.out.print("Additional charge: ");
+                double additionalCharge = sc.nextDouble();
+                // Employee emp = new OutsourcedEmployee(name, hours, valuePerHour,
+                // additionalCharge);
+                list.add(new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge)); // <-------- Same
+                                                                                               // function, just a new
+                                                                                               // way to create.
+            } else {
+                // emp = new Employee(name, hours, valuePerHour);
+                list.add(new Employee(name, hours, valuePerHour)); // <-------- Same function, just a new way to create.
+            }
+
+        }
+
+        System.out.println();
+        System.out.println("PAYMENTS:");
+        for (Employee emp : list) {
+
+        }
     }
 
 }
