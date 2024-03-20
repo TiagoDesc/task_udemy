1da862c7f700698866cb99ef7b9b200787e6fcdf
TiagoDesc <nobretiago96@gmail.com>
2024-03-19 23:57:53 -0300

    new

---
+++ Enumerações/GeometricFigure/Circle.java
@@ -0,0 +1,29 @@
+package Enumerações.GeometricFigure;
+
+public class Circle extends Shape {
+
+    private Double radius;
+
+    public Circle() {
+        super();
+    }
+
+    public Circle(Color color, Double radius) {
+        super(color);
+        this.radius = radius;
+    }
+
+    public Double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(Double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public Double area() {
+        return 3.14 * radius;
+    }
+
+}
--- Enumerações/GeometricFigure/Rectangle.java
+++ Enumerações/GeometricFigure/Rectangle.java
@@ -5,6 +5,16 @@ public class Rectangle extends Shape {
     private Double width;
     private double height;
 
+    public Rectangle() {
+        super();
+    }
+
+    public Rectangle(Color color, Double width, double height) {
+        super(color);
+        this.width = width;
+        this.height = height;
+    }
+
     @Override
     public Double area() {
         return width * height;
