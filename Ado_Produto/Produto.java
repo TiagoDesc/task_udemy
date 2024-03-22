71333ba0087fbea50fdedefc433615cb3a6c6e46
TiagoDesc <nobretiago96@gmail.com>
2024-03-21 23:44:44 -0300

    att

--- Ado_Produto/Produto.java
+++ Ado_Produto/Produto.java
@@ -1,5 +1,42 @@
 package Ado_Produto;
 
-public class Produto {
+public abstract class Produto {
+
+    private String nome;
+    private Double preco;
+    private int quantidade;
+
+    public Produto() {
+    }
+
+    public Produto(String nome, Double preco, int quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
 
 }
