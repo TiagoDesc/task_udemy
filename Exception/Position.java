53dcf93b37c05212fc40bb82cf5688ff1a2dba7d
TiagoDesc <nobretiago96@gmail.com>
2024-06-02 22:14:42 -0300

    Teste

--- Exception/Position.java
+++ Exception/Position.java
@@ -1,8 +1,23 @@
 package Exception;
 
+import java.util.Scanner;
+
 public class Position {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try {
+            System.out.println("Enter the vect names: ");
+            String vect[] = sc.nextLine().split(" ");
+            System.out.println("Enter the position: ");
+            int position = sc.nextInt();
+            System.out.println(vect[position]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid Position!");
+        }
+
+        System.out.println("End of program!");
+        sc.close();
 
     }
 
