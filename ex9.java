aa05ced0c53290143b11e3299810bed08f88e341
TiagoDesc <nobretiago96@gmail.com>
2024-06-07 23:55:51 -0300

    English

--- ex9.java
+++ ex9.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
+
 public class ex9 {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
-        
+        Scanner sc = new Scanner(System.in);
+
         int gasolina = 0;
         int diesel = 0;
         int alcool = 0;
@@ -11,45 +12,42 @@ public class ex9 {
         int cod;
         int total = 0;
 
-        System.out.println("Digite o código: ");
+        System.out.println("Enter the code: ");
         cod = sc.nextInt();
 
-        while(cod != 1 && cod != 2 && cod != 3){
+        while (cod != 1 && cod != 2 && cod != 3) {
 
-        if(cod != 1 && cod != 2 && cod != 3){
-                System.out.println("Número inválido. Digite novamente: ");
+            if (cod != 1 && cod != 2 && cod != 3) {
+                System.out.println("Invalid Number. Enter again: ");
                 cod = sc.nextInt();
             }
         }
 
-        while(cod != 4){
-           
-            if(cod == 1){
+        while (cod != 4) {
+
+            if (cod == 1) {
                 alcool = alcool + 1;
-            }else if(cod == 2){
+            } else if (cod == 2) {
                 gasolina = gasolina + 1;
-            }else if(cod == 3){
+            } else if (cod == 3) {
                 diesel = diesel + 1;
             }
 
-        
-            if(cod != 1 && cod != 2 && cod != 3){
-                System.out.println("Número inválido. Digite novamente: ");
+            if (cod != 1 && cod != 2 && cod != 3) {
+                System.out.println("Invalid Number. Enter again: ");
                 cod = sc.nextInt();
             }
-            System.out.println("Digite o código:");
-            cod = sc.nextInt();  
-        
-}
+            System.out.println("Enter the code:");
+            cod = sc.nextInt();
 
-            
-            System.out.println();
-            System.out.println("Muito obrigado!");
-            System.out.println("Alcool: " + alcool);
-            System.out.println("Gasolina: " + gasolina);
-            System.out.println("Diesel: " + diesel);
+        }
+
+        System.out.println();
+        System.out.println("Thank You!");
+        System.out.println("Alcool: " + alcool);
+        System.out.println("Gas: " + gasolina);
+        System.out.println("Diesel: " + diesel);
 
     }
-    
-}
 
+}
