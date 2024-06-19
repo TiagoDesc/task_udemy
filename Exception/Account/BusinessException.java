eb62024c65290a173bfc4672e66ef523149e2a62
TiagoDesc <nobretiago96@gmail.com>
2024-06-18 21:39:05 -0300

    new class

---
+++ Exception/Account/BusinessException.java
@@ -0,0 +1,10 @@
+package Exception.Account;
+
+public class BusinessException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public BusinessException(String msg) {
+        super(msg);
+    }
+}
