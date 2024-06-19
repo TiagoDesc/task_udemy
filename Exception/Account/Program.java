4a2dc1d2df5d882055c7a0b7ba59cb48c4afcb3f
TiagoDesc <nobretiago96@gmail.com>
2024-06-18 21:06:28 -0300

    First changes

--- Exception/Account/Program.java
+++ Exception/Account/Program.java
@@ -1,9 +1,20 @@
 package Exception.Account;
 
+import java.util.Scanner;
+
 public class Program {
 
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Account number: ");
+        int number = sc.nextInt();
+        System.out.print("Holder: ");
+        String holder = sc.next();
+        sc.nextLine();
+        System.out.print("Balance: ");
+        double balance = sc.nextDouble();
+        System.out.print("Withdraw Limit: ");
+        double withdrawLimit = sc.nextDouble();
     }
 
 }
