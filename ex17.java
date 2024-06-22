c128e980cf694659026058eb8e258599cf6300eb
TiagoDesc <nobretiago96@gmail.com>
2024-06-21 22:17:00 -0300

    close

--- ex17.java
+++ ex17.java
@@ -1,20 +1,24 @@
 import java.util.Scanner;
+
 public class ex17 {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
+        Scanner sc = new Scanner(System.in);
 
         System.out.println("Type the amount of lines and descovery your values atributes: ");
         int n = sc.nextInt();
 
-        for(int i = 1; i <= n; i++){
+        for (int i = 1; i <= n; i++) {
 
             int first = i;
             int second = i * i;
             int third = i * i * i;
 
             System.out.printf("%d, %d, %d.\n", first, second, third);
+
         }
+
+        sc.close();
     }
-    
+
 }
