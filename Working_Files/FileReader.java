c302fc6c79fb7e29a6d2fdb07cb85f69a65dd633
TiagoDesc <nobretiago96@gmail.com>
2024-06-25 23:51:16 -0300

    Fr & Br

--- Working_Files/FileReader.java
+++ Working_Files/FileReader.java
@@ -1,8 +1,16 @@
 package Working_Files;
 
+import java.io.BufferedReader;
+
 public class FileReader {
 
     public static void main(String[] args) {
+
+        String path = "C:\\Users\\n" + //
+                "obre\\OneDrive\\Área de Trabalho\\ws-udemy\\Working_Files\\in.txt";
+
+        FileReader fr = null;
+        BufferedReader br = null;
 
     }
 
