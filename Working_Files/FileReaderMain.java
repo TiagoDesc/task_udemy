41d711a83c87804e00f5910bf89cf681284969ee
TiagoDesc <nobretiago96@gmail.com>
2024-06-26 00:06:27 -0300

    remove all

--- Working_Files/FileReaderMain.java
+++ Working_Files/FileReaderMain.java
@@ -1,25 +1,8 @@
 package Working_Files;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-
 public class FileReaderMain {
 
     public static void main(String[] args) {
 
-        String path = "C:\\Users\\n" + //
-                "obre\\OneDrive\\Área de Trabalho\\ws-udemy\\Working_Files\\in.txt";
-
-        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-            String line = br.readLine();
-            while (line != null) {
-                System.out.println(line);
-                line = br.readLine();
-            }
-        } catch (IOException e) {
-            System.out.println("Error: " + e.getMessage());
-
-        }
     }
 }
