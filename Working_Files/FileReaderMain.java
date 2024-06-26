2eac3394f29d0dd0b742a10b74671aa6c9e3495d
TiagoDesc <nobretiago96@gmail.com>
2024-06-25 23:53:30 -0300

    rename

--- Working_Files/FileReaderMain.java
+++ Working_Files/FileReaderMain.java
@@ -1,16 +1,22 @@
 package Working_Files;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
 
-public class FileReader {
+public class FileReaderMain {
 
     public static void main(String[] args) {
 
         String path = "C:\\Users\\n" + //
                 "obre\\OneDrive\\Área de Trabalho\\ws-udemy\\Working_Files\\in.txt";
-
-        FileReader fr = null;
         BufferedReader br = null;
+        FileReader fr = null;
+
+        try {
+            fr = new FileReader(path);
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
 
     }
 
