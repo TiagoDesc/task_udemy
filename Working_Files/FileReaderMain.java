afed635145e485392d726313feee48b5ad7cc918
TiagoDesc <nobretiago96@gmail.com>
2024-06-26 00:14:36 -0300

    new

--- Working_Files/FileReaderMain.java
+++ Working_Files/FileReaderMain.java
@@ -1,8 +1,27 @@
 package Working_Files;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class FileReaderMain {
 
     public static void main(String[] args) {
+
+        String lines[] = new String[] { "Good Morning", "Good Afternoon", "Good Night" };
+
+        String path = "C:\\Users\\n" + //
+                "obre\\OneDrive\\Área de Trabalho\\ws-udemy\\Working_Files\\out.txt";
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
+            for (String line : lines) {
+                bw.write(line);
+                bw.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
 
     }
 }
