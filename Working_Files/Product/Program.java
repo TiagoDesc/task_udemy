d99d54b743f2ef3f78ecf852a408178ac6fe6ebb
TiagoDesc <nobretiago96@gmail.com>
2024-07-02 00:44:27 -0300

    finish

--- Working_Files/Product/Program.java
+++ Working_Files/Product/Program.java
@@ -1,6 +1,11 @@
 package Working_Files.Product;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -18,7 +23,43 @@ public class Program {
         File sourceFile = new File(sourceFileStr);
         String sourceFolderStr = sourceFile.getParent();
 
-        System.out.println(sourceFileStr);
+        boolean success = new File(sourceFolderStr + "/out").mkdir();
+
+        String targetFileStr = sourceFolderStr + "/input.txt";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(sourceFileStr))) {
+
+            String itemCsv = br.readLine();
+            while (itemCsv != null) {
+
+                String fields[] = itemCsv.split(",");
+                String name = fields[0];
+                double price = Double.parseDouble(fields[1]);
+                int quantity = Integer.parseInt(fields[2]);
+
+                list.add(new Product(name, price, quantity));
+
+                itemCsv = br.readLine();
+            }
+
+            try (BufferedWriter bw = new BufferedWriter(new FileWriter(targetFileStr))) {
+
+                for (Product item : list) {
+                    bw.write(item.getName() + "." + String.format("%.2f", item.totalValue()));
+                    bw.newLine();
+                }
+
+                System.out.println(targetFileStr + "Created");
+
+            } catch (IOException e) {
+                System.out.println("Error writing file: " + e.getMessage());
+
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error writing file: " + e.getMessage());
+
+        }
     }
 
 }
