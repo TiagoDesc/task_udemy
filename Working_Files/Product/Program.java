6f71bc71b94d40efbae9a64b525fbac342fceafb
TiagoDesc <nobretiago96@gmail.com>
2024-07-02 00:29:38 -0300

    att

--- Working_Files/Product/Program.java
+++ Working_Files/Product/Program.java
@@ -1,9 +1,24 @@
 package Working_Files.Product;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Program {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        List<Product> list = new ArrayList<>();
+
+        System.out.println("Enter a file path: ");
+        String sourceFileStr = sc.nextLine();
+
+        File sourceFile = new File(sourceFileStr);
+        String sourceFolderStr = sourceFile.getParent();
+
+        System.out.println(sourceFileStr);
     }
 
 }
