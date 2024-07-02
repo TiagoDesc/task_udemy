e33284b6f8654e52046ceeaf7998d35b2fbdddfc
TiagoDesc <nobretiago96@gmail.com>
2024-07-02 00:00:23 -0300

    finish

--- Working_Files/Path/Path.java
+++ Working_Files/Path/Path.java
@@ -18,6 +18,15 @@ public class Path {
         for (File folder : folders) {
             System.out.println(folder);
         }
+
+        File files[] = path.listFiles(File::isFile);
+        System.out.println("Files: ");
+        for (File file : files) {
+            System.out.println(file);
+        }
+
+        boolean success = new File(strPath + "\\subdir").mkdir();
+        System.out.println("Directory created successfully: " + success);
     }
 
 }
