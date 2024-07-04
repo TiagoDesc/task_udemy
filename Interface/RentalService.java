3374c6bc8516f184a3a0279d157e7c4289f85bc3
TiagoDesc <nobretiago96@gmail.com>
2024-07-04 16:30:53 -0300

    Finished class

---
+++ Interface/RentalService.java
@@ -0,0 +1,64 @@
+package Interface;
+
+import java.time.Duration;
+
+import Interface.Rental.BrazilTaxService;
+import Interface.Rental.CarRental;
+import Interface.Rental.Invoice;
+
+public class RentalService {
+
+    private Double pricePerHour;
+    private Double pricePerDay;
+
+    private BrazilTaxService taxService;
+
+    public RentalService(Double pricePerHour, Double pricePerDay, BrazilTaxService taxService) {
+        this.pricePerHour = pricePerHour;
+        this.pricePerDay = pricePerDay;
+        this.taxService = taxService;
+    }
+
+    public Double getPricePerHour() {
+        return pricePerHour;
+    }
+
+    public void setPricePerHour(Double pricePerHour) {
+        this.pricePerHour = pricePerHour;
+    }
+
+    public Double getPricePerDay() {
+        return pricePerDay;
+    }
+
+    public void setPricePerDay(Double pricePerDay) {
+        this.pricePerDay = pricePerDay;
+    }
+
+    public BrazilTaxService getTaxService() {
+        return taxService;
+    }
+
+    public void setTaxService(BrazilTaxService taxService) {
+        this.taxService = taxService;
+    }
+
+    public void processInvoice(CarRental carRental) {
+
+        double minutes = Duration.between(carRental.getStart(), carRental.getFinish()).toMinutes();
+        double hours = minutes / 60;
+
+        double basicPayment;
+        if (hours <= 12) {
+            basicPayment = pricePerHour * Math.ceil(hours);
+        } else {
+            basicPayment = pricePerDay * Math.ceil(hours / 24);
+        }
+
+        double tax = taxService.tax(basicPayment);
+
+        carRental.setInvoice(new Invoice(basicPayment, tax));
+
+    }
+
+}
