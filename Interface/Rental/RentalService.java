a9c984543e350346d7432295c060904f6bb2af65
TiagoDesc <nobretiago96@gmail.com>
2024-07-05 21:55:02 -0300

    Implements Interface

--- Interface/Rental/RentalService.java
+++ Interface/Rental/RentalService.java
@@ -7,35 +7,11 @@ public class RentalService {
     private Double pricePerHour;
     private Double pricePerDay;
 
-    private BrazilTaxService taxService;
+    private TaxService taxService;
 
-    public RentalService(Double pricePerHour, Double pricePerDay, BrazilTaxService taxService) {
+    public RentalService(Double pricePerHour, Double pricePerDay, TaxService taxService) {
         this.pricePerHour = pricePerHour;
         this.pricePerDay = pricePerDay;
-        this.taxService = taxService;
-    }
-
-    public Double getPricePerHour() {
-        return pricePerHour;
-    }
-
-    public void setPricePerHour(Double pricePerHour) {
-        this.pricePerHour = pricePerHour;
-    }
-
-    public Double getPricePerDay() {
-        return pricePerDay;
-    }
-
-    public void setPricePerDay(Double pricePerDay) {
-        this.pricePerDay = pricePerDay;
-    }
-
-    public BrazilTaxService getTaxService() {
-        return taxService;
-    }
-
-    public void setTaxService(BrazilTaxService taxService) {
         this.taxService = taxService;
     }
 
---
+++ Interface/Rental/TaxService.java
@@ -0,0 +1,7 @@
+package Interface.Rental;
+
+public interface TaxService {
+
+    public double tax(double amount);
+
+}
