c830b911a217c0f4d246de90cc2a498619f61c91
TiagoDesc <nobretiago96@gmail.com>
2024-07-08 23:57:52 -0300

    new commit

---
+++ Interface/Contract/ContractService.java
@@ -0,0 +1,15 @@
+package Interface.Contract;
+
+public class ContractService {
+
+    private OnlinePaymentService onlinePaymentService;
+
+    public ContractService(OnlinePaymentService onlinePaymentService) {
+        this.onlinePaymentService = onlinePaymentService;
+    }
+
+    public void processContract(Contract contract, int months) {
+
+    }
+
+}
--- Interface/Contract/Program.java
+++ Interface/Contract/Program.java
@@ -1,8 +1,16 @@
 package Interface.Contract;
 
+import java.util.Scanner;
+
 public class Program {
 
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the contrat data:");
+        System.out.println("Number: ");
+        int number = sc.nextInt();
+        System.out.println("Date (dd/MM/yyyy): ");
 
     }
 
