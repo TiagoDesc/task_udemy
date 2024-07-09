1475c8fa1e728d4216289eba58b8eb2b116cb778
TiagoDesc <nobretiago96@gmail.com>
2024-07-09 19:54:57 -0300

    finished program

--- Interface/Contract/Program.java
+++ Interface/Contract/Program.java
@@ -1,16 +1,39 @@
 package Interface.Contract;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Program {
 
     public static void main(String[] args) {
 
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the contrat data:");
         System.out.println("Number: ");
         int number = sc.nextInt();
         System.out.println("Date (dd/MM/yyyy): ");
+        LocalDate date = LocalDate.parse(sc.next(), fmt);
+        System.out.print("Contract Value: ");
+        double totalValue = sc.nextDouble();
+
+        Contract obj = new Contract(number, date, totalValue);
+
+        System.out.print("Enter the number of installments: ");
+        int n = sc.nextInt();
+
+        ContractService contractService = new ContractService(new PaypalService());
+
+        contractService.processContract(obj, n);
+
+        System.out.println("Installments:");
+        for (Installment installment : obj.getInstallments()) {
+            System.out.println(installment);
+        }
+
+        sc.close();
 
     }
 
