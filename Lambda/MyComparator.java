f25c6e8086b9da9b265dba0578f147811d8871b5
TiagoDesc <nobretiago96@gmail.com>
2024-08-06 19:27:50 -0300

    finish

--- Lambda/MyComparator.java
+++ Lambda/MyComparator.java
@@ -1,5 +1,11 @@
 package Lambda;
 
-public class MyComparator {
+import java.util.Comparator;
 
+public class MyComparator implements Comparator<Product> {
+
+    @Override
+    public int compare(Product p1, Product p2) {
+        return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+    }
 }
--- Lambda/Program.java
+++ Lambda/Program.java
@@ -1,5 +1,22 @@
 package Lambda;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Program {
 
+    public static void main(String[] args) {
+
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("TV", 900.00));
+        list.add(new Product("Notebook", 1200.00));
+        list.add(new Product("Tablet", 450.00));
+
+        list.sort((p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
+
+        for (Product p : list) {
+            System.out.println(p);
+        }
+    }
 }
