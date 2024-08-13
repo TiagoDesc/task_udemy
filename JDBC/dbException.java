259867a20c17ecb929fdecea06dea373c2943645
TiagoDesc <nobretiago96@gmail.com>
2024-08-13 00:01:07 -0300

    new class

---
+++ JDBC/dbException.java
@@ -0,0 +1,11 @@
+package JDBC;
+
+public class dbException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public dbException(String msg) {
+        super(msg);
+    }
+
+}
