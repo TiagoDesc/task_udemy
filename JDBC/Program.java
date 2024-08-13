4b71e56ab7ef1d65f86b2ee3956eab4bea2aa774
TiagoDesc <nobretiago96@gmail.com>
2024-08-13 00:21:25 -0300

    new

--- JDBC/Program.java
+++ JDBC/Program.java
@@ -1,8 +1,12 @@
 package JDBC;
 
+import java.sql.Connection;
+
 public class Program {
 
     public static void main(String[] args) {
+        Connection conn = DB.getConnection();
 
+        DB.closeConnection();
     }
 }
